8167c248077ee40fd764a89de015bb98c852d86d
Tsunemoriii-3 <157890669+Tsunemoriii-3@users.noreply.github.com>
2025-01-24 01:27:40 +0530

    Update config.py

--- Powers/config.py
+++ Powers/config.py
@@ -6,7 +6,7 @@ from os import getenv
 
 API_ID = 20628383  # This should be without anhhy quotation as it is integer
 API_HASH = "65a242463b8af9ba7b3c41d8de9738d1" #This should be inside the quotation as it is string
-BOT_TOKEN = "7893371295:AAGb_0v0_7qZCR7AIL1BUb5PSGspuAt4iFA" # Same here
+BOT_TOKEN = "7573977444:AAGd75K7xF67Xe0fQmCQCtqHRPEb6enpFwE" # Same here
 SUDO = [int(i.strip()) for i in getenv("SUDO", "1432756163 1344569458 1446111611 682111519").strip().split()]
 OWNER_ID = int(getenv("OWNER","1446111611"))
 FSUB_CHANNEL = [int(i.strip())
@@ -15,7 +15,7 @@ REQ_FSUB = [int(i.strip()) for i in getenv("REQ_FSUB", "").strip().split()]
 AUTO_DEL = int(getenv("AUTO_DEL_TIME", 720))
 AUTO_DEL_IN = getenv("AUTO_DEL_IN", "minute").lower()
 START_PIC = getenv(
-    "START_PIC", "https://envs.sh/g9p.jpg")
+    "START_PIC", "https://envs.sh/gNI.jpg")
 DB_URI = "mongodb+srv://utahheroku10:utahheroku10@cluster0.lt5bp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 SEARCH_PIC = "https://envs.sh/gNT.jpg"
 NO_RES_PIC = "https://envs.sh/gNp.jpg"
